c90a0deef316e13e496676d61fceebd1622205e7
EC2 Default User <ec2-user@ip-172-31-32-245.ap-northeast-1.compute.internal>
2025-09-15 09:23:04 +0000

    [Finish] 4-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -11,14 +12,21 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update
-    redirect_to user_path
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :profile_image)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
   end
 
 end
